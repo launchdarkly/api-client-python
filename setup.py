38dbeccaa80fb5c8f32a5ce830b2f3cef42f7562
LaunchDarklyCI <team@launchdarkly.com>
2018-12-03 23:59:26 +0000

    Version 2.0.10 automatically generated from ld-openapi@ca184b4.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "launchdarkly-api"
-VERSION = "1.0.0"
+VERSION = "2.0.10"
 # To install the library, run the following
 #
 # python setup.py install
